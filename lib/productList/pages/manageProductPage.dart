c3bf0f343da0826180227c9e006ecc40a8cec773
Paritosh Raj <paritoshraj@Paritoshs-MacBook-Air.local>
2019-06-17 11:33:25 +0530

    tab view completed

--- lib/productList/pages/manageProductPage.dart
+++ lib/productList/pages/manageProductPage.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 import '../appBar.dart';
+import "./addproductPage.dart";
+import "./productList.dart";
 
 class ManageProduct extends StatelessWidget {
+  _tabs() {
+    return TabBar(
+      tabs: <Widget>[
+        Tab(
+          text: "Add Product",
+          icon: Icon(Icons.create),
+        ),
+        Tab(
+          text: "Porduct List",
+          icon: Icon(Icons.list),
+        )
+      ],
+    );
+  }
+
+  _tabsView() {
+    return TabBarView(
+      children: <Widget>[AddProduct(), ProductList()],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: CustomAppBar(),
-      appBar: AppBar(
-        title: Text("Manage Product"),
-      ),
-      body: Center(
-        child: Text("Manage Product"),
-      ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          drawer: CustomAppBar(),
+          appBar: AppBar(title: Text("Manage Product"), bottom: _tabs()),
+          body: _tabsView()),
     );
   }
 }

fcf77fe35e6da3bf5cbb9ca7da3e6a6f3a6ed117
Paritosh Raj <paritoshraj@Paritoshs-MacBook-Air.local>
2019-06-18 11:45:39 +0530

    add product form

--- lib/productList/pages/addproductPage.dart
+++ lib/productList/pages/addproductPage.dart
@@ -1,10 +1,83 @@
 import "package:flutter/material.dart";
 
-class AddProduct extends StatelessWidget {
+class AddProduct extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AddProduct();
+  }
+}
+
+class _AddProduct extends State<AddProduct> {
+  String title = "";
+  String description = "";
+  double price = 0.0;
+
+  _onTiteChange(String value) {
+    setState(() {
+      title = value;
+    });
+  }
+
+  _onDescriptionChange(String value) {
+    setState(() {
+      description = value;
+    });
+  }
+
+  _onPriceChange(String value) {
+    setState(() {
+      price = double.parse(value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Add Product page coming soon"),
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Title",
+              icon: Icon(Icons.title),
+            ),
+            onChanged: _onTiteChange,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Description", icon: Icon(Icons.description)),
+            maxLines: 4,
+            onChanged: _onDescriptionChange,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Price", icon: Icon(Icons.monetization_on)),
+            keyboardType: TextInputType.number,
+            onChanged: _onPriceChange,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Add",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                onPressed: () {}),
+          )
+        ],
+      ),
     );
   }
 }

4e3d0afba653edd0e057a35a2838ad7c14c8ce5e
Paritosh Raj <paritoshraj@Paritoshs-MacBook-Air.local>
2019-06-20 09:51:20 +0530

    login page ui improvement

--- lib/productList/pages/authPage.dart
+++ lib/productList/pages/authPage.dart
@@ -5,15 +5,43 @@ class Auth extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.8), BlendMode.dstATop),
+                image: AssetImage("assets/foodbg.jpg"),
+                fit: BoxFit.cover)),
         child: Center(
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            child: Text("Login",
-                style: TextStyle(
-                  color: Colors.white,
-                )),
-            onPressed: () =>
-                Navigator.pushReplacementNamed(context, "/products"),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(left: 15, right: 15),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      filled: true,
+                      fillColor: Colors.white),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      filled: true,
+                      fillColor: Colors.white),
+                ),
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  child: Text("Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
+                  onPressed: () =>
+                      Navigator.pushReplacementNamed(context, "/products"),
+                )
+              ],
+            ),
           ),
         ),
       ),

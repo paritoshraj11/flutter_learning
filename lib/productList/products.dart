1629ab1942d82d11d7a9cea14fca8cd960adf982
Paritosh Raj <paritoshraj@Paritoshs-MacBook-Air.local>
2019-06-20 13:23:18 +0530

    GestureDetector and on tap feature of on the product details

--- lib/productList/products.dart
+++ lib/productList/products.dart
@@ -7,72 +7,71 @@ class Products extends StatelessWidget {
   Products(this.products);
 
   Widget _listItem(BuildContext context, int index) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.only(top: 10),
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-              products[index]["image"],
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    products[index]["title"],
-                    style: TextStyle(
-                        fontFamily: "Oswald",
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Theme.of(context).accentColor),
-                    child: Text(
-                      '₹ ${products[index]["price"].toString()}',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )
-                ],
+    return GestureDetector(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                products[index]["image"],
               ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Colors.grey,
-                      width: 0.5,
-                      style: BorderStyle.solid)),
-              child: Text("Sector 49, Gurgaon"),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.info),
-                  onPressed: () =>
-                      Navigator.pushNamed(context, "/product-detail/$index")
-                          .then((value) {
-                        print(value);
-                      }),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          products[index]["title"],
+                          style: TextStyle(
+                              fontFamily: "Oswald",
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Theme.of(context).accentColor),
+                          child: Text(
+                            '₹ ${products[index]["price"].toString()}',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    )),
+                    IconButton(
+                      padding: EdgeInsets.only(right: 10),
+                      icon: Icon(Icons.favorite_border),
+                      onPressed: () {},
+                    )
+                  ],
                 ),
-                IconButton(
-                  color: Theme.of(context).primaryColor,
-                  icon: Icon(Icons.favorite_border),
-                  onPressed: () {},
-                )
-              ],
-            )
-          ],
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey,
+                        width: 0.5,
+                        style: BorderStyle.solid)),
+                child: Text("Sector 49, Gurgaon"),
+              ),
+            ],
+          ),
         ),
       ),
+      onTap: () =>
+          Navigator.pushNamed(context, "/product-detail/$index").then((value) {
+            print(value);
+          }),
     );
   }
 

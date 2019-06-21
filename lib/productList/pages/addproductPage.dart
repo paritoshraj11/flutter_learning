1c65f4da4bd54aac436524a82d243bf8fdccef54
Paritosh Raj <paritoshraj@Paritoshs-MacBook-Air.local>
2019-06-21 10:09:47 +0530

    form key and keyboard dismiss on tap of other part of ui

--- lib/productList/pages/addproductPage.dart
+++ lib/productList/pages/addproductPage.dart
@@ -13,6 +13,7 @@ class _AddProduct extends State<AddProduct> {
   String title = "";
   String description = "";
   double price = 0.0;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   _onTiteChange(String value) {
     setState(() {
@@ -33,6 +34,10 @@ class _AddProduct extends State<AddProduct> {
   }
 
   _onSave() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
     final Map<String, dynamic> product = {
       "title": title,
       "description": description,
@@ -48,54 +53,69 @@ class _AddProduct extends State<AddProduct> {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 550 ? 500 : deviceWidth;
     final double targetPadding = deviceWidth - targetWidth;
-    return Container(
-      padding: EdgeInsets.all(15.0),
-      width: 100,
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: "Title",
-              icon: Icon(Icons.title),
-            ),
-            onChanged: _onTiteChange,
-          ),
-          TextField(
-            decoration: InputDecoration(
-                labelText: "Description", icon: Icon(Icons.description)),
-            maxLines: 4,
-            onChanged: _onDescriptionChange,
-          ),
-          TextField(
-            decoration: InputDecoration(
-                labelText: "Price", icon: Icon(Icons.monetization_on)),
-            keyboardType: TextInputType.number,
-            onChanged: _onPriceChange,
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            child: RaisedButton(
-                color: Theme.of(context).primaryColor,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "Add",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    )
-                  ],
+    return GestureDetector(
+      child: Container(
+          padding: EdgeInsets.all(15.0),
+          width: 100,
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Title",
+                    icon: Icon(Icons.title),
+                  ),
+                  onSaved: _onTiteChange,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Title is required";
+                    }
+                  },
                 ),
-                onPressed: _onSave),
-          )
-        ],
-      ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Description", icon: Icon(Icons.description)),
+                  maxLines: 4,
+                  onSaved: _onDescriptionChange,
+                  //onChanged: _onDescriptionChange,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Price", icon: Icon(Icons.monetization_on)),
+                  keyboardType: TextInputType.number,
+                  onSaved: _onPriceChange,
+                  // onChanged: _onPriceChange,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: RaisedButton(
+                      color: Theme.of(context).primaryColor,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Add",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      onPressed: _onSave),
+                )
+              ],
+            ),
+          )),
+      onTap: () {
+        print("on tap called !!!!!");
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
     );
   }
 }
